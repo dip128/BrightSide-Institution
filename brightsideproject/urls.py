237d8125a391cfda6a9d77538c7f1fd5f536b2fe
Diptom Saha <sahadiptom128@gmail.com>
2020-08-13 11:11:55 -0700

    Third Commit

--- brightsideproject/urls.py
+++ brightsideproject/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+admin.site.site_header = "BrightSide Institute Admin"
+admin.site.site_title = " BrightSide Admin Portal"
+admin.site.index_title = "Welcome to BrightSide Institute Admin Portal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include('home.urls'))
